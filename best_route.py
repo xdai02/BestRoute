95f6b2352de44ba8ec37e325daa37e96abbfdd5b
xdai02 <2513276112@qq.com>
2023-10-04 12:43:14 +0800

    list all possible routes

--- best_route.py
+++ best_route.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import permutations
 
 
 def graph_create():
@@ -21,10 +22,53 @@ def graph_create():
     return graph
 
 
+def get_route(graph, locations):
+    total_duration = 0
+    total_price = 0.0
+
+    for i in range(len(locations) - 1):
+        start, end = locations[i], locations[i + 1]
+        for next_stop, next_duration, next_price in graph.get(start, []):
+            if next_stop == end:
+                total_duration += next_duration
+                total_price += next_price
+                break
+        else:
+            return None, None
+
+    return total_duration, total_price
+
+
 def main():
     graph = graph_create()
-    print(graph)
 
+    start_location = "Singapore (SIN)"
+    
+    # List of locations to visit
+    desired_locations = [
+        "Sydney (SYD)",
+        "Cairns (CNS)",
+        "Melbourne (MEL)",
+    ]
+
+    all_permutations = permutations(desired_locations)
+    route_id = 1
+
+    for perm in all_permutations:
+        full_route = [start_location] + list(perm) + [start_location]
+        total_duration, total_price = get_route(graph, full_route)
+        
+        if total_duration is not None:
+            hours = total_duration // 60
+            minutes = total_duration % 60
+
+            print(f"Route ID: {route_id}")
+            print(f"Route: {' -> '.join(full_route)}")
+            print(f"Total Duration: {hours} hours {minutes} minutes")
+            print(f"Total Price: ${total_price:.2f}")
+            print("-"*60)
+
+        route_id += 1
 
 if __name__ == "__main__":
     main()
